9256dce4c1a4e241060dd7596fb0058821208600
karimSATPRO <107763973+karimSATPRO@users.noreply.github.com>
2022-12-09 01:12:13 +0100

    Update oscam.sh

--- oscam.sh
+++ oscam.sh
@@ -1,7 +1,17 @@
+echo "############### Cccams gratuit #################"
+echo "############ BY WOLRD OF ENIGMA2 #################"
 #!/bin/sh
 #
 
 wget -O /etc/tuxbox/config/oscam.server "https://raw.githubusercontent.com/karimSATPRO/Cccam/main/oscam.server"
 
+echo ""
+cd ..
+sync
+echo "############ INSTALLATION COMPLETED ########"
+echo "############ RESTARTING... #################" 
+init 4
+sleep 2
+init 3
 exit 0
 
