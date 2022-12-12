4d44aec958041dfe575a0c197363bccf64b99c6f
karimSATPRO <107763973+karimSATPRO@users.noreply.github.com>
2022-12-12 14:25:50 +0100

    Update CCcam.sh

--- CCcam.sh
+++ CCcam.sh
@@ -1,1 +1,16 @@
+echo "############### Cccams gratuit #################"
+echo "############ BY KARIM ABU RIDA  #################"
+#!/bin/sh
+#
+
 wget -O /etc/tuxbox/config/CCcam.sfg "https://raw.githubusercontent.com/karimSATPRO/Cccam/main/CCcam.cfg"
+
+echo ""
+cd ..
+sync
+echo "############ INSTALLATION COMPLETED ########"
+echo "############ RESTARTING... #################" 
+init 4
+sleep 2
+init 3
+exit 0
